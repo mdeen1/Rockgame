9d5ecb02cfa4bf346e0bc6ebf1718e15dfb8a156
mdeen1 <34013848+mdeen1@users.noreply.github.com>
2018-08-27 17:03:36 -0700

    working on the javascript

--- app.js
+++ app.js
@@ -1,3 +1,35 @@
+const userScore = 0;
+const computerScore = 0;
+const userScore_span =  document.getElementById("user-score");
+const ComputerScore_span =  document.getElementById("computer-score");
+const scoreBoard_div =  document.querySelector(".score-board");
+const result_div =  document.querySelector(".result");
+const rock_div =  document.getElementById("r");
+const paper_div =  document.getElementById("p");
+const scissors_div =  document.getElementById("s");
 
+function getComputerChoice() {
+    const choices = ['r', 'p', 's'];
+    const randomNumber = Math.floor(Math.random() 
+* 3);
+    return choices(randomNumber);
+}
+console.log(getComputerChoice());
+
+function game(userChoice) {
+    
+}
+
+rock_div.addEventListener('click', function() {
+    console.log("hey you clicked on rock");
+})
+
+paper_div.addEventListener('click', function() {
+    console.log("hey you clicked on paper");
+})
+
+scissors_div.addEventListener('click', function() {
+    console.log("hey you clicked on scissors");
+})
 
     
