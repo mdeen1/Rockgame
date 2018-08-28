a813d8a596758df0d565794077090ef5ec6e0263
mdeen1 <34013848+mdeen1@users.noreply.github.com>
2018-08-28 04:23:23 -0700

    Add files via upload

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-const userScore = 0;
+let userScore = 0;
 const computerScore = 0;
 const userScore_span =  document.getElementById("user-score");
 const ComputerScore_span =  document.getElementById("computer-score");
 const scoreBoard_div =  document.querySelector(".score-board");
-const result_div =  document.querySelector(".result");
+const result_p =  document.querySelector(".result > p");
 const rock_div =  document.getElementById("r");
 const paper_div =  document.getElementById("p");
 const scissors_div =  document.getElementById("s");
@@ -14,13 +14,46 @@ function getComputerChoice() {
 * 3);
     return choices(randomNumber);
 }
-console.log(getComputerChoice());
 
-function game(userChoice) {
-    
+function convertToWord(letter) {
+    if (letter === "r") return "Rock";
+    if (letter === "p") return "Paper";
+    return "Scissors";
 }
 
-rock_div.addEventListener('click', function() {
+function win(userChoice, computerChoice) {
+    userScore ++;
+    userScore_span.innerHTML = userScore;
+    ComputerScore_span.innerHTML = computerScore;
+    result_p.innerHTML = convertToWord(userChoice) + "beats" + convertToWord(computerChoice) + ". You win!"
+}
+
+
+function game(userChoice) {
+   const computerChoice = getComputerChoice();
+    switch (userChoice + computerChoice) {
+            case "rs";
+            case "pr";
+            case "sp";
+                win(userChoice,  computerChoice);
+            break;
+            case "rp";
+            case "ps";
+            case "sr";
+                lose(userChoice,  computerChoice);
+            break;
+            case "rr";
+            case "pp";
+            case "ss";
+                draw(userChoice,  computerChoice);
+            break;
+    }
+    }
+    }
+}
+
+function main(){
+   rock_div.addEventListener('click', function() {
     console.log("hey you clicked on rock");
 })
 
@@ -30,6 +63,9 @@ paper_div.addEventListener('click', function() {
 
 scissors_div.addEventListener('click', function() {
     console.log("hey you clicked on scissors");
-})
+}) 
+}
+
+
 
     
